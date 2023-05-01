5090d96735e2365df05bd7ead9fc6ea0f8de6ae0
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-01 00:00:50 -0400

    More styleguide

--- bazelrio_gentool/generate_module_project_files.py
+++ bazelrio_gentool/generate_module_project_files.py
@@ -1,6 +1,7 @@
 import os
 from bazelrio_gentool.utils import render_template, write_file, TEMPLATE_BASE_DIR
 from bazelrio_gentool.load_cached_versions import load_cached_version_info
+from bazelrio_gentool.cli import GenericCliArgs
 
 
 class MandetoryDependencySetting:
@@ -17,37 +18,41 @@ class MandetoryDependencySetting:
         return f"MandetoryDependencySetting: {self.repo_name}, {self.version}, {self.use_local_version}"
 
 
-def create_default_mandatory_settings(
-    use_local_roborio,
-    use_local_bazelrio,
-    use_local_rules_pmd,
-    use_local_rules_checkstyle,
-    use_local_rules_wpiformat,
-):
+def create_default_mandatory_settings(generic_cli: GenericCliArgs):
     default_rules_roborio_toolchain = MandetoryDependencySetting(
         "rules_bzlmodrio_toolchains",
         "2023-7",
-        use_local_roborio,
+        generic_cli.use_local_roborio,
     )
     default_rules_bazelrio = MandetoryDependencySetting(
         "rules_bazelrio",
         "0.0.9",
-        use_local_bazelrio,
+        generic_cli.use_local_bazelrio,
     )
     default_rules_pmd = MandetoryDependencySetting(
         "rules_pmd",
         "6.39.0",
-        use_local_rules_pmd,
+        generic_cli.use_local_rules_pmd,
     )
     default_rules_checkstyle = MandetoryDependencySetting(
         "rules_checkstyle",
         "10.1",
-        use_local_rules_checkstyle,
+        generic_cli.use_local_rules_checkstyle,
+    )
+    default_rules_spotless = MandetoryDependencySetting(
+        "rules_spotless",
+        "2022.30",
+        generic_cli.use_local_rules_spotless,
     )
     default_rules_wpiformat = MandetoryDependencySetting(
         "rules_wpiformat",
         "2022.30",
-        use_local_rules_wpiformat,
+        generic_cli.use_local_rules_wpiformat,
+    )
+    default_rules_wpi_styleguide = MandetoryDependencySetting(
+        "rules_wpi_styleguide",
+        "2022.30",
+        generic_cli.use_local_rules_wpi_styleguide,
     )
 
     return MandatoryDependencySettings(
@@ -56,7 +61,9 @@ def create_default_mandatory_settings(
         rules_bazelrio=default_rules_bazelrio,
         rules_pmd=default_rules_pmd,
         rules_checkstyle=default_rules_checkstyle,
+        rules_spotless=default_rules_spotless,
         rules_wpiformat=default_rules_wpiformat,
+        rules_wpi_styleguide=default_rules_wpi_styleguide,
     )
 
 
@@ -67,7 +74,9 @@ class MandatoryDependencySettings:
         rules_bazelrio,
         rules_pmd,
         rules_checkstyle,
+        rules_spotless,
         rules_wpiformat,
+        rules_wpi_styleguide,
         bcr_branch="main",
     ):
         self.bcr_branch = bcr_branch
@@ -75,7 +84,9 @@ class MandatoryDependencySettings:
         self.rules_bazelrio = rules_bazelrio
         self.rules_pmd = rules_pmd
         self.rules_checkstyle = rules_checkstyle
+        self.rules_spotless = rules_spotless
         self.rules_wpiformat = rules_wpiformat
+        self.rules_wpi_styleguide = rules_wpi_styleguide
 
 
 def generate_module_project_files(module_directory, group, mandetory_dependencies, no_roborio=False):
@@ -104,12 +115,6 @@ def generate_module_project_files(module_directory, group, mandetory_dependencie
         "tests/MODULE.bazel",
         "tests/WORKSPACE.bzlmod",
         "tests/WORKSPACE",
-        "tests/styleguide/BUILD.bazel",
-        "tests/styleguide/cc_styleguide.bzl",
-        "tests/styleguide/private/BUILD.bazel",
-        "tests/styleguide/private/download_styleguide_deps.bzl",
-        "tests/styleguide/private/load_styleguide_deps.bzl",
-        "tests/styleguide/private/setup_styleguide.bzl",
         
     ]
 
@@ -139,10 +144,6 @@ def generate_module_project_files(module_directory, group, mandetory_dependencie
     if group.java_deps:
         for tf in [
             "maven_java_deps.bzl",
-            "tests/styleguide/checkstyle-suppressions.xml",
-            "tests/styleguide/checkstyle.xml",
-            "tests/styleguide/java_styleguide.bzl",
-            "tests/styleguide/pmd-ruleset.xml",
             
             ]:
             template_file = os.path.join(TEMPLATE_BASE_DIR, "module", tf + ".jinja2")
